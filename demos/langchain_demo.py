2917e1640022fa3d1ec75307bcd44be1b185ff7f
oneone <onelee85@gmail.com>
2026-04-27 15:02:32 +0800

    feat(langchain): enhance demo with output parsers and chaining
    Add StrOutputParser, JsonOutputParser and RunnableLambda to improve output handling. Implement a chain for processing medical queries with structured JSON output and follow-up treatment suggestions.

--- demos/langchain_demo.py
+++ demos/langchain_demo.py
@@ -1,10 +1,15 @@
 # 基于本地 LLM langchain demo
-import openai
+from typing import Any
+
+
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.runnables import RunnableLambda
 
 load_dotenv()
 
@@ -33,8 +38,24 @@ messages = [
     ("human", "我想知道什么是原因")
 ]
 
-prompt_template = PromptTemplate.from_template("你是一个专业的{system_prompt}")
+str_output_parser = StrOutputParser()
+json_output_parser = JsonOutputParser()
+runnable_lambda = RunnableLambda[Any, Any](lambda x: {"reasoning": x.content})
 
-prompt_text = prompt_template.format(system_prompt="医生")
-response = llm.invoke(prompt_text)
-print(response.content)
+
+prompt_template = PromptTemplate.from_template(
+    "你好， 我口腔张不开了，你是一个专业的{system_prompt}, 我想知道什么病症的名称？ 并返回 JSON 格式返回，key 是 reasoning，value 是病症")
+
+prompt_template2 = PromptTemplate.from_template(
+    "我的病症是 {reasoning}, 为我开药建议")
+
+
+# prompt_text = prompt_template.format(system_prompt="医生")
+# response = llm.invoke(prompt_text)
+# print(response.content)
+
+# 原理： Runnable 是一个可调用的对象，它接受一个输入并返回一个输出
+chain = prompt_template | llm | runnable_lambda | prompt_template2 | llm | str_output_parser
+response: str = chain.stream({"system_prompt": "医生"})
+for chunk in response:
+    print(chunk, end="", flush=True)
